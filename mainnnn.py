82312cdf627cc17bfb1ed17a856ca1cf8ba649ca
SeydaYalavac <seydayal@gmail.com>
2026-04-24 10:45:51 +0300

    Remove unused files and IDE metadata from ortak-yeni

--- mainnnn.py
+++
@@ -1,5 +0,0 @@
-# main.py içeriği
-from config import BASE_ADDRESS  # Ayarları al
-from face_utils import recognize_face  # Kişi 1'den fonksiyon çağır
-from bellek_utils import get_user_authority  # Kişi 2'den fonksiyon çağır
-from access_control import check_access  # Kişi 3'den fonksiyon çağır
